a0c55e980d22cc02615a841e42fef1a474fbf704
QTpierogi <yohanzarembo@gmail.com>
2025-01-17 09:10:35 +0500

    Modal Input Secured

--- src/TodosPage.tsx
+++ src/TodosPage.tsx
@@ -1,23 +1,34 @@
-import { useState, useMemo } from 'react'
+import { useState, useMemo, useCallback, memo } from 'react'
+import Modal from './modal/index'
 
 interface ITodoItem {
     id: number
     text: string
 }
 
-const RemovableTodosPage = () => {
-    const [todoList, setTodoList] = useState < ITodoItem[] > ([
-        { id: 1, text: 'First' },
-        { id: 2, text: 'Second' },
-        { id: 3, text: 'Third' },
-        { id: 4, text: 'Fourth' },
-    ])
+const initialTodos = [
+    { id: 1, text: 'First' },
+    { id: 2, text: 'Second' },
+    { id: 3, text: 'Third' },
+    { id: 4, text: 'Fourth' },
+]
 
-    const handleRemoveTodo = (id: ITodoItem['id']) => {
-        setTodoList((prev) => prev.filter((it) => it.id !== id))
-    }
+const RemovableTodosPage = () => {
+    const [isCreateTodoModalOpen, setIsCreateTodoModalOpen] = useState(false)
+
+    const [todoList, setTodoList] = useState<ITodoItem[]>(initialTodos)
 
     const handleCreateVacation = () => {
+        setIsCreateTodoModalOpen(true);
+        //setTodoList((prev) => [...prev, { id: prev[prev.length - 1].id + 1, text: taskText }])
+    }
+
+    const handleRemoveTodo = useCallback((id: ITodoItem['id']) => {
+        setTodoList((prev) => prev.filter((it) => it.id !== id))
+    }, [])
+
+    const handleSaveTask = () => {
+        setIsCreateTodoModalOpen(false);
         setTodoList((prev) => [...prev, { id: prev[prev.length - 1].id + 1, text: taskText }])
     }
 
@@ -32,12 +43,7 @@ const RemovableTodosPage = () => {
     }, [todoList, searchString])
 
     const renderTodo = (item: ITodoItem, _: number) => {
-        return (
-            <li className="todoItem" key={item.id}>
-                <p>{item.text}</p>
-                <button onClick={() => handleRemoveTodo(item.id)}>x</button>
-            </li>
-        )
+        return <TodoItem onRemove={handleRemoveTodo} item={item} key={item.id} />
     }
 
     return (
@@ -51,18 +57,37 @@ const RemovableTodosPage = () => {
                     onChange={(ev) => setSearchString(ev.target.value)}
                 />
                 <ul className="todoListWrapper">{searchResults.map(renderTodo)}</ul>
-                <div className="addItemWithText">
-                    <input
-                        className="newItemText"
-                        placeholder="Add Task..."
-                        value={taskText}
-                        onChange={(ev) => setText(ev.target.value)}
-                    />
+                <div className="todoCreateWrapper">
                     <button onClick={handleCreateVacation}>+ Item</button>
+                    {isCreateTodoModalOpen &&
+                        <Modal>
+                            <h2>Creating element</h2>
+                            <input
+                                className="newItemText"
+                                placeholder="Add Task..."
+                                value={taskText}
+                                onChange={(ev) => setText(ev.target.value)}
+                            />
+                            <button onClick={() => handleSaveTask()}>Save</button>
+                            <button onClick={() => setIsCreateTodoModalOpen(false)}>Close</button>
+                        </Modal>}
                 </div>
             </div>
         </div>
     )
 }
+
+interface ITodoItemProps {
+  item: ITodoItem
+  onRemove: (id: ITodoItem['id']) => void
+}
+const TodoItem = memo((props: ITodoItemProps) => {
+  return (
+    <li className="todoItem" key={props.item.id}>
+      <p>{props.item.text}</p>
+      <button onClick={() => props.onRemove(props.item.id)}>x</button>
+    </li>
+  )
+})
 
 export default RemovableTodosPage
--- src/modal/index.tsx
+++ src/modal/index.tsx
@@ -1,12 +1,13 @@
 import css from './modal.module.css'
 import { createPortal } from 'react-dom'
+import { PropsWithChildren } from 'react'
 
-const Modal = () => {
+const Modal = (props: PropsWithChildren) => {
     const body = document.getElementsByTagName('body')[0]
 
     return createPortal(
         <div className={css.shadow}>
-            <div className={css.modal}>{this.props.children}</div>
+            <div className={css.modal}>{props.children}</div>
         </div>,
         body
     )
